468928befae94cb7c30049855bac34051a9db7e5
Vitaliy Galiy <galiy.vitaliyy@gmail.com>
2024-09-17 22:53:16 +0300

    Create super_user, create dynamick models (delete hardcode)

--- my_site/my_site/blog/admin.py
+++ my_site/my_site/blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Author, Post, Tag
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name",)
+
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ("author", "tags", "date",)
+    list_display = ("title", "date", "author",)
+    prepopulated_fields = {"slug": ("title",)}
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Tag)
 
 # Register your models here.
--- my_site/my_site/blog/models.py
+++ my_site/my_site/blog/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from datetime import date
+# from django.urls import reverse
+# from django.utils.text import slugify
+from django.core.validators import MinLengthValidator
 
+
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.caption}"
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60)
+    email_field = models.EmailField(max_length=242)
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        verbose_name_plural = "Authors"
+
+class Post(models.Model):
+    title = models.CharField(max_length=80)
+    excerpt = models.CharField(max_length=200)
+    image_name = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True, blank=True, null=True)
+    slug = models.SlugField(unique=True, default="", blank = True, db_index=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name='posts')
+    tags = models.ManyToManyField(Tag)
+
+    # def save(self, *args, **kwargs):
+    #     self.slug = slugify(self.title)
+    #     super().save(*args, **kwargs)
+
+    # def __str__(self):
+    #     return f"{self.title} {self.author.first_name}"
+
+
+    
 # Create your models here.
